16fc51550411fcc9c4d877555238bc9be726f67a
DavidBP014 <juanbp014@gmail.com>
2024-08-06 20:12:13 -0500

    add new

--- 0x03-asynchronous/4-get_sum.dart
+++ 0x03-asynchronous/4-get_sum.dart
@@ -1,53 +1,28 @@
-import 'dart:async';
 import 'dart:convert';
-
-Future<String> fetchUserOrders(String id) async {
-  var orders = {
-    "7ee9a243-01ca-47c9-aa14-0149789764c3": ["pizza", "orange"]
-  };
-  try {
-    return Future.delayed(
-        const Duration(seconds: 2), () => json.encode(orders[id]));
-  } catch (err) {
-    return "error caught : $err";
-  }
-}
-
-Future<String> fetchUserData() => Future.delayed(
-      const Duration(seconds: 2),
-      () =>
-          '{"id" : "7ee9a243-01ca-47c9-aa14-0149789764c3", "username" : "admin"}',
-    );
-
-Future<String> fetchProductPrice(product) async {
-  var products = {"pizza": 20.30, "orange": 10, "water": 5, "soda": 8.5};
-  try {
-    return Future.delayed(
-        const Duration(seconds: 2), () => json.encode(products[product]));
-  } catch (err) {
-    return "error caught : $err";
-  }
-}
+import '4-util.dart';
 
 Future<double> calculateTotal() async {
   try {
-    String userData = await fetchUserData();
-    final Map<String, dynamic> userMap = jsonDecode(userData);
-    String userId = userMap['id'];
+    // Fetch user data
+    final userData = await fetchUserData();
+    final user = json.decode(userData);
+    final userId = user['id'];
 
-    String ordersData = await fetchUserOrders(userId);
-    List<dynamic> ordersList = jsonDecode(ordersData);
+    // Fetch user orders
+    final ordersData = await fetchUserOrders(userId);
+    final List<dynamic> orders = json.decode(ordersData);
 
-    double total = 0.0;
-    for (var order in ordersList) {
-      String product = order.toString();
-      String priceData = await fetchProductPrice(product);
-      double productPrice = double.tryParse(priceData) ?? 0.0;
-      total += productPrice;
+    // Calculate total price
+    double total = 0;
+    for (var product in orders) {
+      final priceData = await fetchProductPrice(product);
+      final price = json.decode(priceData);
+      total += price;
     }
 
     return total;
-  } catch (error) {
+  } catch (e) {
+    // If any error occurs, return -1
     return -1;
   }
 }
