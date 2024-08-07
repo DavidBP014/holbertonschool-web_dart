5e5a693ff18b34e890781d154844c98440259614
DavidBP014 <juanbp014@gmail.com>
2024-08-06 20:14:00 -0500

    defv

--- 0x03-asynchronous/3-greet_user.dart
+++ 0x03-asynchronous/3-greet_user.dart
@@ -1,36 +1,15 @@
-import 'dart:async';
+// 3-util.dart
+
 import 'dart:convert';
 
+// Simulates fetching user data from an API
 Future<String> fetchUserData() => Future.delayed(
-      const Duration(seconds: 2),
-      () =>
-          '{"id" : "7ee9a243-01ca-47c9-aa14-0149789764c3", "username" : "admin"}',
-    );
+  const Duration(seconds: 2),
+  () => '{"id" : "7ee9a243-01ca-47c9-aa14-0149789764c3", "username" : "admin"}',
+);
 
-Future<String> greetUser() async {
-  try {
-    String userData = await fetchUserData();
-    final Map<String, dynamic> userMap = jsonDecode(userData);
-    String username = userMap['username'];
-    return 'Hello $username';
-  } catch (error) {
-    return 'error caught: $error';
-  }
-}
-
-Future<bool> checkCredentials() =>
-    Future.delayed(const Duration(seconds: 2), () => true);
-
-Future<String> loginUser() async {
-  try {
-    bool credentialsValid = await checkCredentials();
-    print('There is a user: $credentialsValid');
-    if (credentialsValid) {
-      return await greetUser();
-    } else {
-      return 'Wrong credentials';
-    }
-  } catch (error) {
-    return 'error caught: $error';
-  }
-}
+// Simulates checking user credentials, returning true if valid
+Future<bool> checkCredentials() => Future.delayed(
+  const Duration(seconds: 2),
+  () => true,
+);
